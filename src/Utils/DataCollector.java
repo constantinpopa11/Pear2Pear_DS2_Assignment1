e4c8485e87633da47f2823c19db551bbe62ffda5
Panino_MCS <andrea.traldi@unitn.it>
2020-11-24 16:06:58 +0100

    Fixed isolation saving process; implemented discovery of distant nodes

--- src/Utils/DataCollector.java
+++ src/Utils/DataCollector.java
@@ -28,10 +28,12 @@ public class DataCollector {
 	        pw2.close();
 	        fw2.close();
 	        
+	        
+	        
 	        //clear IsolatedRelays
 	        FileWriter fw3 = new FileWriter("IsolatedRelays.csv", false);
 			PrintWriter pw3 = new PrintWriter(fw3, false);
-			pw3.write("ID;Tick" + '\n');
+			pw3.write("Count;Tick" + '\n');
 	        pw3.flush();
 	        pw3.close();
 	        fw3.close();
@@ -55,16 +57,37 @@ public class DataCollector {
 		}
 	}
 	
-	public static void saveIsolation(int id, double tick, String fileName) {
-		CSVWriter writer;
+	public static void saveIsolation(double tick, String fileName) {
+			
 		try {
-			writer = new CSVWriter(new FileWriter(fileName, true), ';', '\0');
-			String[] entries = {id + "",  tick + ""};
-			writer.writeNext(entries);
+			CSVReader reader = new CSVReader(new FileReader(fileName), ';', '\0');
+			CSVWriter writer = new CSVWriter(new FileWriter(fileName, true), ';', '\0');
+			String[] entries = {"1",  tick + ""};
+			List<String[]> allElements = reader.readAll();
+			reader.close();
+			
+			if(allElements.size() != 1 && tick == Double.parseDouble((allElements.get(allElements.size()-1))[1])) {
+				double toBeIncremented = Double.parseDouble((allElements.get(allElements.size()-1))[0]);
+				toBeIncremented++;
+				allElements.get(allElements.size()-1)[0] = toBeIncremented + "";
+			} else {
+				allElements.add(entries);
+			}
+			
+			FileWriter fw3 = new FileWriter(fileName, false);
+			PrintWriter pw3 = new PrintWriter(fw3, false);
+	        pw3.flush();
+	        pw3.close();
+	        fw3.close();
+			
+			
+			writer.writeAll(allElements);
 			writer.close();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 }
